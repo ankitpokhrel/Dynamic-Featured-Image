d1c617fddb7db4e26b33f71798aaf9b939b05bdc
Ankit Pokhrel <ankitpokhrel@gmail.com>
2014-02-13 23:45:51 +0545

    New media uploader

--- js/script-dfi.js
+++ js/script-dfi.js
@@ -9,7 +9,8 @@ jQuery(document).ready(function($){
 	/*
 	 * Add new meta box
 	 */
-	$(document).on('click', '.dfiAddNew', function(){	   		
+	$(document).on('click', '.dfiAddNew', function() {
+		   		
        var obj = $(this);
        var id = parseInt( $('.featured-meta-box:last').find('.dfiAddNew').attr('data-id') );
        
@@ -21,7 +22,8 @@ jQuery(document).ready(function($){
        var metaBoxContentObj = newMetaBox.find('.inside');
        metaBoxContentObj.html('');
        obj.hide();
-       obj.parent().append('<img src="images/wpspin_light.gif" class="dfiLoading">').hide().fadeIn(200);       
+       obj.parent().append('<img src="images/wpspin_light.gif" class="dfiLoading">').hide().fadeIn(200);       
+
        $.ajax({
           type: 'POST',  
           url: 'admin-ajax.php',  
@@ -43,94 +45,102 @@ jQuery(document).ready(function($){
 	/*
 	 * Remove featured image meta box
 	 */
-	$(document).on('click', '.dfiRemove', function(){
-	   if( confirm('Are you sure?') ){
+	$(document).on('click', '.dfiRemove', function() {
+
+	   if( confirm('Are you sure?') ) {
+
 	     var dfiMetaBox = $(this).closest('.featured-meta-box');	     
 	     var totalMetaBox = $('.featured-meta-box').length;
 	     
-	     if( totalMetaBox == 1 ){	          
+	     if( totalMetaBox == 1 ) {
+
 	           dfiMetaBox.find('.dfiImg').attr('src', '');
 	           dfiMetaBox.find('.dfiImageHolder').val('');
 	           dfiMetaBox.find('.dfiFeaturedImage')
 	                     .removeClass('hasFeaturedImage')
 	                     .show()
-	                     .animate({ opacity: 1, display: 'inline-block' }, 600);	                     
+	                     .animate({ opacity: 1, display: 'inline-block' }, 600);	
+
 	     } else {
+
 		      dfiMetaBox.fadeOut(500, function(){
 		        $(this).remove();  
 		      });
+
 		 }
+
 	   }
+
 	});
 	
 	/*
-	 * Select featured image from media library
-	 */
-	
+	 * Display media editor and allow to select featured image from the media library
+	 */	
 	var restore_send_to_editor = "";
-	$(document).on('click', '.dfiFeaturedImage', function() {		
+	$(document).on('click', '.dfiFeaturedImage', function() {
+
 		current = $(this);
 		
 		var post_id = current.attr('data-post-id');
 		
-		restore_send_to_editor = window.send_to_editor;
-		if( null != current){
-		    media_uploader();
-		    tb_show('', 'media-upload.php?post_id=' + post_id + '&amp;type=image&amp;TB_iframe=true');
+		restore_send_to_editor = wp.media.editor.send.attachment;
+		if( null != current){		    
+
+		    wp.media.editor.send.attachment = function(props, attachment) {
+
+	    		var fullSize = imgUrl = attachment.url;
+	    		var imgUrlTrimmed, fullUrlTrimmed;
+	    	
+	    		switch( props.size ) {
+	    			case 'thumbnail':
+	    					imgUrl = attachment.sizes.thumbnail.url;
+	    					break;
+
+	    			case 'medium':
+	    					imgUrl = attachment.sizes.medium.url;
+	    					break;
+
+	    			case 'large':
+	    					imgUrl = attachment.sizes.large.url;	    					
+	    		}
+
+	    		imgUrlTrimmed = imgUrl.split('wp-content');
+	        	imgUrlTrimmed = '/wp-content' + imgUrlTrimmed[1];
+
+	        	fullUrlTrimmed = fullSize.split('wp-content');
+	        	fullUrlTrimmed = '/wp-content' + fullUrlTrimmed[1];
+
+	    		var featuredBox = current.parent(); 
+	    		
+	    		featuredBox.find('.fImg').attr({
+	    			'src': imgUrl,
+	    			'data-src': fullSize
+	    		});
+	    		
+	    		featuredBox.find('.dfiFeaturedImage').addClass('hasFeaturedImage');
+	    			
+	    		var dfiFeaturedImages = [imgUrlTrimmed, fullUrlTrimmed];
+	    		
+	    		featuredBox.find('img').attr('src', attachment.sizes.medium.url).fadeIn(200);
+	    		featuredBox.find('input.dfiImageHolder').val(dfiFeaturedImages);
+
+			}
+
+			wp.media.editor.open(this);
 		}
+		
 		return false;
+
 	});
-	 
-	/*
-	 * Allow access to media uploader
-	 */
-    function media_uploader(){
-       	window.send_to_editor = function(html){       	        	             
-    		var fullSize = $('img', html).parent().attr('href');    		
-    		if( typeof fullSize === "undefined" ) fullSize = $(html).attr('src');
-    			
-    		var imgUrl, imgUrlTrimmed, fullUrlTrimmed;
-    		
-    		if( /wp-content/.test(fullSize) ){
-        		imgUrl = $('img', html).attr('src');
-        		
-        		imgUrlTrimmed = imgUrl.split('wp-content');
-        		imgUrlTrimmed = '/wp-content' + imgUrlTrimmed[1];
-        		
-        		fullUrlTrimmed = fullSize.split('wp-content');
-        		fullUrlTrimmed = '/wp-content' + fullUrlTrimmed[1];
-    		} else {
-    		    imgUrl = fullSize;
-    		    imgUrlTrimmed = fullSize;
-    		    fullUrlTrimmed = fullSize;
-    		}
-    		
-    		var featuredBox = current.parent();    		
-    		
-    		featuredBox.find('.fImg').attr({
-    			'src': imgUrl,
-    			'data-src': fullSize
-    		});
-    		
-    		featuredBox.find('.dfiFeaturedImage').addClass('hasFeaturedImage');
-    			
-    		var dfiFeaturedImages = [imgUrlTrimmed, fullUrlTrimmed];
-    		
-    		featuredBox.find('img').attr('src', imgUrl).fadeIn(200);
-    		featuredBox.find('input.dfiImageHolder').val(dfiFeaturedImages);
-    		tb_remove();
-    		window.send_to_editor = restore_send_to_editor;
-	 } 
-   }
    
-	/*
+	/**
 	 * Enable toggle of dynamically generated featured box
 	 */
-	$(document).on('click', '.dfiDynamicBox', function(){
+	$(document).on('click', '.dfiDynamicBox', function() {
 	    $(this).parent().toggleClass('closed');
 	});
 	
-	/*
+	/**
 	 * Add a hover animation in image
 	 */
 	$(document).on({
@@ -145,4 +155,7 @@ jQuery(document).ready(function($){
 	        obj.find('.hasFeaturedImage').fadeOut(100);        	        
 	    }
 	}, '.featured-meta-box .inside');
+
 });
+
+//END

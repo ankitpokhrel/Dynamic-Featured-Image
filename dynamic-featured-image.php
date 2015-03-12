17f0738b96074423bd8736dc5a412cbf57ffd5cd
Ankit Pokhrel <ankitpokhrel@gmail.com>
2015-03-12 23:14:03 +0545

    Remove extra quotation mark from anchor

--- dynamic-featured-image.php
+++ dynamic-featured-image.php
@@ -296,7 +296,7 @@ class Dynamic_Featured_Image
 	public function featured_meta_box($post, $featured)
 	{
 		$featuredImg = $featured['args'][0];
-		$featuredId    = is_null( $featured['args'][1]) ? 2 : --$featured['args'][1];
+		$featuredId    = is_null( $featured['args'][1] ) ? 2 : --$featured['args'][1];
 
 		$featuredImgTrimmed = $featuredImgFull = $featuredImg;
 		if ( !is_null( $featuredImg ) ) {
@@ -349,7 +349,7 @@ class Dynamic_Featured_Image
 		return "<a href='javascript:void(0)' class='dfiFeaturedImage {$hasFeaturedImage}' title='". __('Set Featured Image', $this->_textDomain) . "' data-post-id='" . get_the_ID() . "'><span class='dashicons dashicons-camera'></span></a><br/>
 			<img src='" . $thumbnail . "' class='dfiImg {$dfiEmpty}'/>
 			<div class='dfiLinks'>
-				<a href='javascript:void(0)'' data-id='{$featuredId}' data-id-local='" . self::_get_number_translation( ($featuredId + 1) ) . "' class='dfiAddNew dashicons dashicons-plus' title='" . __('Add New', $this->_textDomain) ."'></a>
+				<a href='javascript:void(0)' data-id='{$featuredId}' data-id-local='" . self::_get_number_translation( ($featuredId + 1) ) . "' class='dfiAddNew dashicons dashicons-plus' title='" . __('Add New', $this->_textDomain) ."'></a>
 				<a href='javascript:void(0)' class='dfiRemove dashicons dashicons-minus' title='" . __('Remove', $this->_textDomain) . "'></a>
 			</div>
 			<div class='dfiClearFloat'></div>

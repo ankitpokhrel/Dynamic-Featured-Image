1fd217cbb6ed1ac16dfb691ab8b0e0d494695ae6
Ankit Pokhrel <ankitpokhrel@gmail.com>
2015-03-21 01:00:53 +0545

    Test ajax request

--- tests/test-dynamic-featured-image-ajax.php
+++ tests/test-dynamic-featured-image-ajax.php
@@ -1,43 +1,49 @@
 <?php
-
+/**
+ * Test DFI ajax requests
+ *
+ * @group ajax
+ */
 class DynamicFeaturedImageAjaxTest extends WP_Ajax_UnitTestCase
 {
 
 	private $__post_id = null;
 
-	protected $_dfi = null;
-
 	public function setUp() 
 	{
 		parent::setUp();
 
-		$this->_dfi = new Dynamic_Featured_Image;
-
 		$this->__post_id = $this->factory->post->create( array( 'post_title' => 'Dynamic Featured Image WordPress Plugin' ) );
 	}
 
+	/**
+	 * @covers Dynamic_Featured_Image::ajax_callback
+	 */
 	public function testAjaxCallback()
 	{
 		$this->_setRole('administrator');
 
-		$expectedOutput = "<a href='javascript:void(0)' class='dfiFeaturedImage hasFeaturedImage' title='Set Featured Image' data-post-id='" . $this->__post_id . "'><span class='dashicons dashicons-camera'></span></a><br/>
-			<img src='' class='dfiImg '/>
-			<div class='dfiLinks'>
-				<a href='javascript:void(0)' data-id='{$this->__post_id}' data-id-local='" . ($this->__post_id + 1) . "' class='dfiAddNew dashicons dashicons-plus' title='Add New'></a>
-				<a href='javascript:void(0)' class='dfiRemove dashicons dashicons-minus' title='Remove'></a>
-			</div>
-			<div class='dfiClearFloat'></div>
-			<input type='hidden' name='dfiFeatured[]' value='/2015/03/dfi-150x150.jpg'  class='dfiImageHolder' />";
-
-		$this->expectOutputString($expectedOutput);
+		$expectedOutput = 'a href="javascript:void(0)" class="dfiFeaturedImage" title="Set Featured Image"><span class="dashicons dashicons-camera"></span></a><br/>
+				 <img src="" class="dfiImg dfiImgEmpty"/>
+				 <div class="dfiLinks">
+				<a href="javascript:void(0)" data-id="' . $this->__post_id . '" data-id-local="' . ($this->__post_id + 1) . '" class="dfiAddNew dashicons dashicons-plus" title="Add New"></a>
+				<a href="javascript:void(0)" class="dfiRemove dashicons dashicons-minus" title="Remove"></a>
+				 </div>
+				 <div class="dfiClearFloat"></div>
+				 <input type="hidden" name="dfiFeatured[]" value="" class="dfiImageHolder" />';
 
 		$_POST['id'] = $this->__post_id;
-		$this->_handleAjax( 'dfiMetaBox_callback' );
+		try {
+			$this->_handleAjax( 'dfiMetaBox_callback' );
+		} catch ( WPAjaxDieContinueException $e ) {}
+
+		$this->assertTrue( isset($e) );
+		$this->assertEquals( '1', $e->getMessage() );
+		$this->assertContains($expectedOutput, $this->_last_response);
 	}
 
 	public function tearDown()
 	{
 		unset($this->__post_id);
-		unset($this->_dfi);
 	}
 }

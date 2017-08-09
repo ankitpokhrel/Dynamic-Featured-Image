3913e632e090fea5d840d59f29482ffe39797c28
Ankit Pokhrel <ankitpokhrel@gmail.com>
2017-08-09 22:21:20 +0700

    phpcs uninstall script

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,16 @@
 <?php
-	if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-		exit();
-	}
+/**
+ * Uninstall script.
+ *
+ * @since 0.0.0
+ * @package dynamic-featured-image
+ * @author Ankit Pokhrel <info@ankitpokhrel.com, https://ankitpokhrel.com>
+ */
 
-	//cleanup plugin data
-	delete_post_meta_by_key('dfiFeatured');
-	delete_post_meta_by_key('_dfi_link_to_image');
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+// cleanup plugin data.
+delete_post_meta_by_key( 'dfiFeatured' );
+delete_post_meta_by_key( '_dfi_link_to_image' );

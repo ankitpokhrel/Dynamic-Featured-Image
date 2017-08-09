504b27175452f1f30c02aba328d80173e21d699a
Ankit Pokhrel <ankitpokhrel@gmail.com>
2017-08-09 23:31:05 +0700

    phpcs js

--- js/script-dfi.js
+++ js/script-dfi.js
@@ -1,93 +1,91 @@
 /**
- * @file script-dfi.js
- *
  * Script for dynamic featured image plugin.
  *
- * Copyright (c) 2013, Ankit Pokhrel <ankitpokhrel@gmail.com, http://ankitpokhrel.com.np>
+ * @package dynamic-featured-image
+ * @subpackage js
+ *
+ * Copyright (c) 2013, Ankit Pokhrel <info@ankitpokhrel.com, https://ankitpokhrel.com>
  */
 
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function ( $ ) {
     var current = null;
 
-    /**
-     * Add new meta box
-     */
-    $(document).on('click', '.dfiAddNew', function() {
+    // Add new meta box.
+    $( document ).on( 'click', '.dfiAddNew', function () {
 
-        var obj = $(this),
-            id = parseInt( $('.featured-meta-box:last').find('.dfiAddNew').data('id'), 10 ),
-            idLocal = $('.featured-meta-box:last').find('.dfiAddNew').attr('data-id-local'),
-            newMetaBox = obj.closest('.featured-meta-box').clone();
+        var obj = $( this ),
+        id = parseInt( $( '.featured-meta-box:last' ).find( '.dfiAddNew' ).data( 'id' ), 10 ),
+        idLocal = $( '.featured-meta-box:last' ).find( '.dfiAddNew' ).attr( 'data-id-local' ),
+        newMetaBox = obj.closest( '.featured-meta-box' ).clone();
 
-        newMetaBox.find('.hndle span').html( WP_SPECIFIC.metabox_title + " " + idLocal );
-        newMetaBox.attr('id', 'dfiFeaturedMetaBox' + "-" + (++id) );
-        newMetaBox.find('.handlediv').addClass('dfiDynamicBox');
+        newMetaBox.find( '.hndle span' ).html( WP_SPECIFIC.metabox_title + " " + idLocal );
+        newMetaBox.attr( 'id', 'dfiFeaturedMetaBox' + "-" + (++ id) );
+        newMetaBox.find( '.handlediv' ).addClass( 'dfiDynamicBox' );
 
-        var metaBoxContentObj = newMetaBox.find('.inside');
-        metaBoxContentObj.html('');
+        var metaBoxContentObj = newMetaBox.find( '.inside' );
+        metaBoxContentObj.html( '' );
         obj.hide();
-        obj.parent().append('<span class="dfiLoading"></span>').hide().fadeIn(200);
+        obj.parent().append( '<span class="dfiLoading"></span>' ).hide().fadeIn( 200 );
 
-        $.ajax({
+        $.ajax( {
             type: 'POST',
             url: 'admin-ajax.php',
             data: { action: 'dfiMetaBox_callback', id: id },
-            success: function(response){
-                metaBoxContentObj.append(response);
-                newMetaBox.appendTo( obj.closest('.featured-meta-box').parent() );
+            success: function ( response ) {
+                metaBoxContentObj.append( response );
+                newMetaBox.appendTo( obj.closest( '.featured-meta-box' ).parent() );
 
-                //Add post id
-                newMetaBox.find('.dfiFeaturedImage').attr('data-post-id', obj.parent().parent().find('.dfiFeaturedImage').attr('data-post-id') );
+                // Add post id.
+                newMetaBox.find( '.dfiFeaturedImage' ).attr( 'data-post-id',
+                obj.parent().parent().find( '.dfiFeaturedImage' ).attr( 'data-post-id' ) );
 
                 var alias = obj;
-                obj.parent().find('.dfiLoading').fadeOut(300, function(){ $(this).remove(); alias.fadeIn(200); });
+                obj.parent().find( '.dfiLoading' ).fadeOut( 300, function () {
+                    $( this ).remove();
+                    alias.fadeIn( 200 );
+                } );
             }
-        });
+        } );
 
-    });
+    } );
 
-    /**
-     * Remove featured image meta box
-     */
-    $(document).on('click', '.dfiRemove', function() {
+    // Remove featured image meta box.
+    $( document ).on( 'click', '.dfiRemove', function () {
 
-        if( confirm('Are you sure?') ) {
+        if ( confirm( 'Are you sure?' ) ) {
 
-            var dfiMetaBox = $(this).closest('.featured-meta-box'),
-                totalMetaBox = $('.featured-meta-box').length;
+            var dfiMetaBox = $( this ).closest( '.featured-meta-box' ),
+            totalMetaBox = $( '.featured-meta-box' ).length;
 
-            if( totalMetaBox === 1 ) {
+            if ( totalMetaBox === 1 ) {
 
-                dfiMetaBox.find('.dfiImg').attr('src', '');
-                dfiMetaBox.find('.dfiImageHolder').val('');
-                dfiMetaBox.find('.dfiFeaturedImage')
-                        .removeClass('hasFeaturedImage')
-                        .show()
-                        .animate({ opacity: 1, display: 'inline-block' }, 600);
+                dfiMetaBox.find( '.dfiImg' ).attr( 'src', '' );
+                dfiMetaBox.find( '.dfiImageHolder' ).val( '' );
+                dfiMetaBox.find( '.dfiFeaturedImage' )
+                .removeClass( 'hasFeaturedImage' )
+                .show()
+                .animate( { opacity: 1, display: 'inline-block' }, 600 );
 
             } else {
 
-                dfiMetaBox.fadeOut(500, function(){
-                    $(this).remove();
-                });
+                dfiMetaBox.fadeOut( 500, function () {
+                    $( this ).remove();
+                } );
 
             }
 
         }
 
-    });
+    } );
 
-    /**
-     * Display custom media uploader and allow to
-     * select featured image from the media library
-     */
-    $(document).on('click', '.dfiFeaturedImage', function() {
+    // Display custom media uploader and allow to select featured image from the media library.
+    $( document ).on( 'click', '.dfiFeaturedImage', function () {
 
-        current = $(this);
+        current = $( this );
 
-        if( null !== current) {
+        if ( null !== current ) {
 
-            var dfi_uploader = wp.media({
+            var dfi_uploader = wp.media( {
 
                 title: WP_SPECIFIC.mediaSelector_title,
                 button: {
@@ -95,69 +93,64 @@ jQuery(document).ready(function($) {
                 },
                 multiple: false,
 
-            }).on('select', function() {
+            } ).on( 'select', function () {
 
-                var attachment = dfi_uploader.state().get('selection').first().toJSON(),
-                    fullSize = attachment.url,
-                    imgUrl = (typeof attachment.sizes.thumbnail === "undefined") ? fullSize : attachment.sizes.thumbnail.url,
-                    imgUrlTrimmed, fullUrlTrimmed;
+                var attachment = dfi_uploader.state().get( 'selection' ).first().toJSON(),
+                fullSize = attachment.url,
+                imgUrl = (typeof attachment.sizes.thumbnail === "undefined") ? fullSize : attachment.sizes.thumbnail.url,
+                imgUrlTrimmed, fullUrlTrimmed;
 
-                imgUrlTrimmed = imgUrl.replace(WP_SPECIFIC.upload_url, "");
-                fullUrlTrimmed = fullSize.replace(WP_SPECIFIC.upload_url, "");
+                imgUrlTrimmed = imgUrl.replace( WP_SPECIFIC.upload_url, "" );
+                fullUrlTrimmed = fullSize.replace( WP_SPECIFIC.upload_url, "" );
 
                 var featuredBox = current.parent();
 
-                featuredBox.find('.fImg').attr({
+                featuredBox.find( '.fImg' ).attr( {
                     'src': imgUrl,
                     'data-src': fullSize
-                });
+                } );
 
-                featuredBox.find('.dfiFeaturedImage').addClass('hasFeaturedImage');
+                featuredBox.find( '.dfiFeaturedImage' ).addClass( 'hasFeaturedImage' );
 
                 var dfiFeaturedImages = [imgUrlTrimmed, fullUrlTrimmed];
 
                 /**
-                 * Check if medium sized image exists
+                 * Check if medium sized image exists.
+                 *
                  * @type object
                  */
                 var medium = attachment.url;
-                if( typeof attachment.sizes.medium !== "undefined" ) {
+                if ( typeof attachment.sizes.medium !== "undefined" ) {
                     medium = attachment.sizes.medium.url;
                 }
 
-                featuredBox.find('img').attr('src', medium).fadeIn(200);
-                featuredBox.find('input.dfiImageHolder').val(dfiFeaturedImages);
+                featuredBox.find( 'img' ).attr( 'src', medium ).fadeIn( 200 );
+                featuredBox.find( 'input.dfiImageHolder' ).val( dfiFeaturedImages );
 
-            }).open();  
-        }
+            } ).open();
+        } // End if().
 
         return false;
 
-    });
+    } );
 
-    /**
-     * Enable toggle of dynamically generated featured box
-     */
-    $(document).on('click', '.dfiDynamicBox', function() {
-        $(this).parent().toggleClass('closed');
-    });
+    // Enable toggle of dynamically generated featured box.
+    $( document ).on( 'click', '.dfiDynamicBox', function () {
+        $( this ).parent().toggleClass( 'closed' );
+    } );
 
-    /**
-     * Add a hover animation in image
-     */
-    $(document).on({
-        mouseenter: function(){ 
-            var obj = $(this).closest('.featured-meta-box');
-            obj.find('.dfiImg').stop(true, true).animate({ opacity: 0.3 }, 300 );
-            obj.find('.hasFeaturedImage').fadeIn(200);
+    // Add a hover animation in image.
+    $( document ).on( {
+        mouseenter: function () {
+            var obj = $( this ).closest( '.featured-meta-box' );
+            obj.find( '.dfiImg' ).stop( true, true ).animate( { opacity: 0.3 }, 300 );
+            obj.find( '.hasFeaturedImage' ).fadeIn( 200 );
         },
-            mouseleave: function(){
-            var obj = $(this);
-            obj.find('.dfiImg').stop(true, true).animate({ opacity: 1 }, 300 );
-            obj.find('.hasFeaturedImage').fadeOut(100);
+        mouseleave: function () {
+            var obj = $( this );
+            obj.find( '.dfiImg' ).stop( true, true ).animate( { opacity: 1 }, 300 );
+            obj.find( '.hasFeaturedImage' ).fadeOut( 100 );
         }
-    }, '.featured-meta-box .inside');
+    }, '.featured-meta-box .inside' );
 
-});
-
-//END
+} );
